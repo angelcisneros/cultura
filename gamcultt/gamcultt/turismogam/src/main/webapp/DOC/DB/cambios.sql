2ea108aa46821bc6f3de9e9d60329405d9f24f90
Lap_christo <Lap_christo@192.168.1.82>
2015-06-06 19:37:39 -0500

    casas

--- gamcultt/gamcultt/turismogam/src/main/webapp/DOC/DB/cambios.sql
+++ gamcultt/gamcultt/turismogam/src/main/webapp/DOC/DB/cambios.sql
@@ -1,4 +1,7 @@
--- Cambios a la base
-ALTER TABLE `gamcultt`.`categoria_taller` 
-ADD COLUMN `descripcion` VARCHAR(1000) NOT NULL AFTER `nombre`;
 
+
+
+
+-- Cambios a la base
+ALTER TABLE 'gamcultt'.'categoria_taller' 
+ADD COLUMN 'descripcion' VARCHAR(1000) NOT NULL AFTER 'nombre';
