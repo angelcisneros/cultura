bcc8a64dc191bc7da16681bbe5d07b4d88d539cd
Angel Cisneros <skangelescom@gmail.com>
2015-06-15 11:56:06 -0500

    mas coasas

--- gamcultt/gamcultt/turismogam/src/main/java/mx/ipn/escom/turismoazcapotzalco/controller/crud/reportes/AlumnoReporteController.java
+++ gamcultt/gamcultt/turismogam/src/main/java/mx/ipn/escom/turismoazcapotzalco/controller/crud/reportes/AlumnoReporteController.java
@@ -5,32 +5,22 @@
  */
 package mx.ipn.escom.turismoazcapotzalco.controller.crud.reportes;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 import mx.ipn.escom.entidades.Alumno;
-import mx.ipn.escom.entidades.AlumnoClase;
 import mx.ipn.escom.servicios.AlumnoServicio;
-import static mx.ipn.escom.servicios.util.MensajesCrud.ERROR_DATOS;
-import static mx.ipn.escom.servicios.util.MensajesCrud.SESION_CADUCA;
-import mx.ipn.escom.servicios.util.Validaciones;
 import static mx.ipn.escom.servicios.util.Validaciones.esEntero;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -52,7 +42,7 @@ public class AlumnoReporteController {
         if (esEntero(idAlumno)) {
             Alumno alumno = alumnoServicio.buscarPorId(Integer.parseInt(idAlumno));
             response.setContentType("application/application/vnd.ms-excel");
-            response.setHeader("Content-Disposition", "attachment; filename=\""+ alumno.getNombre() + ".xls\"");
+            response.setHeader("Content-Disposition", "attachment; filename=\"" + alumno.getNombre() + ".xls\"");
             PrintWriter out;
             try {
                 out = response.getWriter();
@@ -69,6 +59,44 @@ public class AlumnoReporteController {
                 Logger.getLogger(AlumnoReporteController.class.getName()).log(Level.SEVERE, null, ex);
             }
 
+        }
+    }
+
+    @RequestMapping(value = "reporteAlumnoController/", method = RequestMethod.GET)
+    public void reporteAlumnoController(HttpSession session,
+            HttpServletRequest request, HttpServletResponse response) {
+        if (session.getAttribute("usuario") == null) {
+            return;
+        }
+        List<Alumno> alumnos = alumnoServicio.buscarTodos();
+        response.setContentType("application/application/vnd.ms-excel");
+        response.setHeader("Content-Disposition", "attachment; filename=\"alumnos.xls\"");
+        PrintWriter out;
+        try {
+            out = response.getWriter();
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title></title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Reporte de Alumno Algo</h1>");
+            out.println("<table>");
+            out.println("<thead>");
+            out.println("</thead>");
+            out.println("<tbody>");
+            for (Alumno alumno : alumnos) {
+                out.println("<tr>");
+                out.println("<td>" + alumno.getNombre() + "</td>");
+                out.println("<td>" + alumno.getPaterno() + "</td>");
+                out.println("</tr>");
+            }
+            out.println("</tbody>");
+            out.println("</table>");
+            out.println("</body>");
+            out.println("</html>");
+        } catch (IOException ex) {
+            Logger.getLogger(AlumnoReporteController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
